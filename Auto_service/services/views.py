4267f7f17ddfc2dfc4355306ebf568e012092445
Matutozi <sobowalegz2@gmail.com>
2024-09-09 21:57:17 +0100

    added the urls for the service app

--- Auto_service/services/views.py
+++ Auto_service/services/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from .models import ServiceRequest
+from .forms import ServiceRequestForm
 
-# Create your views here.
+def create_service_request(request):
+    if request.method == 'POST':
+        form = ServiceRequest(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("Service Request Valid")
+        else:
+            return HttpResponse("Error during service request")
+    else:
+        form = ServiceRequestForm()
+    return render(request, 'create_service_request.html', {'form': form})
