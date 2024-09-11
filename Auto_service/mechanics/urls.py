d3d69bbe1a72a982a9536b9ca6d8924adcf7a658
Matutozi <sobowalegz2@gmail.com>
2024-09-11 15:28:45 +0100

    FIX to the login to work as expected

--- Auto_service/mechanics/urls.py
+++ Auto_service/mechanics/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('dashboard/', views.mechanic_dashboard, name='mechanic_dashboard'),
+        path('login/', views.mechanic_login, name='mechanic_login'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
--- Auto_service/mechanics/views.py
+++ Auto_service/mechanics/views.py
@@ -1,4 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from accounts.forms import LoginForm
+
+
+def mechanic_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('mechanic_dashboard')
+            else:
+                messages.error(request, 'Invalid username or password.')
+    else:
+        form = LoginForm()
+    return render(request, 'mechanic_login.html', {'form': form})
 
 def mechanic_dashboard(request):
     return render(request, 'mechanic_dashboard.html')
